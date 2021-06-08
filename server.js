afbe1b3b73a1737f7d12aa742c0d4e0c374ae14f
Ishraq-R-Rahman <67277219+Ishraq-R-Rahman@users.noreply.github.com>
2021-06-09 00:43:38 +0600

    :art:Loan Module Added 3 routes 1) get all loans 2) post create a loan 3) get a loan by its id

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const app = express();
-const User = require("./db/models/userModel") ;
-const LoanRequest = require("./db/models/LoanRequestModel");
+
 
 const PORT = process.env.PORT || 5000;
 
 /** Require Routes */
 const authRouter = require('./routes/auth')
+const loanRouter = require('./routes/loan')
+
 
 // Conncet to the database
 const { mongoose } = require('./db/mongoose');
@@ -50,6 +51,8 @@ require("./passportConfig")(passport);
 
 
 app.use('/api/auth', authRouter );
+app.use('/api/loans', loanRouter);
+
 
 
 
@@ -57,49 +60,6 @@ app.use('/api/auth', authRouter );
 //     if(req.user)
 //     res.send(req.user);// The req.user stores the entire user that has been authenticated inside of it.
 //     else res.send("Unauthorized");
-// });
-
-
-
-
-
-// app.get("/getLoanRequests",(req,res)=>{
-//     LoanRequest.find({}, function(err, requests) {
-//         let req_Map = {};
-
-//         requests.forEach(function(request) {
-//             req_Map[request._id] = request;
-//         });
-
-//         res.json(requests);
-//     });
-// });
-
-// app.post('/createLoanRequest', async(req,res) =>{
-//     console.log(req.user);
-//     let user;
-//     User.findOne({ username: req.user },(err,doc)=>{
-//        user = doc;
-//        console.log(user);
-//     });
-//     const newRequest = new LoanRequest({
-//         Receiver: user,
-//         Amount: req.body.Amount,
-//         Details: req.body.Details,
-//         Status: "New"
-//     });
-//     await newRequest.save();
-//     res.send("Successfully Created LoanRequest");
-
-// });
-
-
-
-// app.get("/loanRequest/:id",async (req,res)=>{
-//         const data = await LoanRequest.findById(req.params.id);
-//         console.log(data);
-//         res.status(200).json(data);
-
 // });
 
 
